64b361634f5496691d3bb6592da484ac3dae5ac9
Muhamad Thoriq Ziyadatul Iman <74279079+mthoriqzi@users.noreply.github.com>
2022-01-13 13:31:26 +0700

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform{
   backend "s3" {
-    bucket = "agriaku-terraform-state"
-    key = "dev_/terraform_state"
+    bucket = "pipelineoik"
+    key = "dev_${var.cloud_name}/terraform_state"
     region = "ap-southeast-1"
   }
 }
